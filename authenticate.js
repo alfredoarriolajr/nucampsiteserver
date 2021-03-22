f36ff2513a0c4cff94586552c090aff2eea58e99
alfredoarriolajr <alfredoarriolajr@gmail.com>
2021-03-22 14:00:39 -0700

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -11,6 +11,17 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin) {
+        return next();
+    }
+    else {
+        const err = new Error ('You are not authorized to perform this operation!')
+        err.status = 403;
+        return next(err)
+    }
+};
+
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 };
